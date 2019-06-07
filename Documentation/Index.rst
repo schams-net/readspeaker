d02784415664d578f348770737cc4f1e0843de5e
Michael Schams <typo3@2019.schams.net>
2019-06-07 16:15:14 +1000

    PoC: unauthorized data manipulation

---
+++ Documentation/Index.rst
@@ -0,0 +1,36 @@
+.. include:: Includes.txt
+
+.. _start:
+
+=============================================================
+ReadSpeaker - Online Text to Speech
+=============================================================
+
+:Extension key:
+   readspeaker
+
+:Version:
+   |release|
+
+:Language:
+   en
+
+:Copyright:
+   2012-2015
+
+:Authors:
+   Oliver Hader
+
+:Status:
+   final
+
+**TYPO3**
+
+   The content of this document is related to TYPO3 CMS,
+   a GNU/GPL CMS/Framework available from `typo3.org
+   <https://typo3.org/>`_ .
+
+
+**Extension Manual**
+
+   This documentation is for the TYPO3 extension EXT:readspeaker.
---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "readspeaker".
+ *
+ * Auto generated 22-04-2013 11:33
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'ReadSpeaker - Online Text to Speech',
+	'description' => 'This extension allows to integrate the ReadSpeaker service (http://www.readspeaker.com/) into TYPO3.',
+	'category' => 'fe',
+	'author' => 'Oliver Hader',
+	'author_email' => 'oliver.hader@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.3.3',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
